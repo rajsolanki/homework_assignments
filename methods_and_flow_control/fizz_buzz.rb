f9e2f8739093590b496afca2dcb23fcc56c47d3a
Raj Solanki <rajmahaj@gmail.com>
2014-06-29 14:17:18 -0600

    completed initial code for fizz_buzz.rb, but want to fina nother method beside for.

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,6 +1,10 @@
 for x in 0..99 do
-  if (x + 1) % 3 == 0
+  if (x + 1) % 15 == 0
+    puts 'FizzBuzz'
+  elsif (x + 1) % 3 == 0
     puts 'Fizz'
+  elsif (x + 1) % 5 == 0
+    puts 'Buzz'
   else puts x + 1
   end
 end
