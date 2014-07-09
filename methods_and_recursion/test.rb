e13d568b8b4edc55c64c58d653615761a8aae410
Raj Solanki <rajmahaj@gmail.com>
2014-07-08 23:16:50 -0600

    remove test.rb

--- methods_and_recursion/test.rb
+++
@@ -1,6 +0,0 @@
-noun1 = 'cat'
-noun2 = 'apple'
-article1 = noun1.start_with?('a', 'e', 'i', 'o', 'u') ? 'an' : 'a'
-article2 = noun2.start_with?('a', 'e', 'i', 'o', 'u') ? 'an' : 'a'
-
-puts "#{article1} #{noun1}, #{article2} #{noun2}"
