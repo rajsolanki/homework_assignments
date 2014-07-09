aa2a690fc9b0766eac0257701d65f0a4f58e473e
Raj Solanki <rajmahaj@gmail.com>
2014-07-09 01:40:13 -0600

    string_practice.rb revision

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -1,11 +1,11 @@
-puts 'this is a string to practice with'
-puts 'this is a string to practice with'.capitalize
-puts 'this is a string to practice with'.upcase
-puts 'this is a string to practice with'.gsub('string', "'string'")
+#!/usr/bin/env ruby
 
-the_string = 'this is a string to practice with'
+string = 'this is a string to practice with'
 
-puts "The string '#{the_string}' has #{the_string.count(the_string)} characters"
-puts 'this is a string to practice with'.reverse
-puts 'this is a string to practice with'
-     .gsub(the_string, 'practice ' * 2 + 'practice!')
+puts string
+puts string.capitalize
+puts string.upcase
+puts string.gsub('string', "'string'")
+puts string.gsub(string, "The string '#{string}' has #{string.count(string)} characters")
+puts string.reverse
+puts string.gsub(string, 'practice ' * 2 + 'practice!')
