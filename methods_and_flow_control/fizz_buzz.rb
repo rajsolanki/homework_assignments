b968541771f560e7716508f6a2b0f5d9a0637d75
Raj Solanki <rajmahaj@gmail.com>
2014-07-09 01:25:32 -0600

    revision to fizz_buzz.rb to use each method and made file executable

--- methods_and_flow_control/fizz_buzz.rb
+++ methods_and_flow_control/fizz_buzz.rb
@@ -1,12 +1,14 @@
-for x in 0..99 do
+#!/usr/bin/env ruby
+
+(1..100).each do |number|
   # Divisible by 15 had to be first
   # in order to get FizzBuzz
-  if (x + 1) % 15 == 0
+  if number % 15 == 0
     puts 'FizzBuzz'
-  elsif (x + 1) % 3 == 0
+  elsif number % 3 == 0
     puts 'Fizz'
-  elsif (x + 1) % 5 == 0
+  elsif number % 5 == 0
     puts 'Buzz'
-  else puts x + 1
+  else puts number
   end
 end
