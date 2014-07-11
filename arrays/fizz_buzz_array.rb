d044da6deeada0850f3565f81ba533b33d1ce80e
Raj Solanki <rajmahaj@gmail.com>
2014-07-11 14:20:28 -0600

    fixx_buzz_array.rb homework

---
+++ arrays/fizz_buzz_array.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+numbers = * (1..100)
+fizzbuzz = %w(Fizz Buzz FizzBuzz)
+
+numbers.map! do |number|
+  if number % 15 == 0
+    numbers.to_s.replace(fizzbuzz[2])
+  elsif number % 3 == 0
+    numbers.to_s.replace(fizzbuzz[0])
+  elsif number % 5 == 0
+    numbers.to_s.replace(fizzbuzz[1])
+  else
+    number
+  end
+end
+
+puts numbers
+
+# Originally, had the following code, but Rubocop
+# didn't like it because of too many conditions and
+# wanted if if/else instead:
+
+# numbers.map! { |number|
+#   (number % 15 == 0) ? fizzbuzz[2] :
+#   (number % 3 == 0) ? fizzbuzz[0] :
+#   (number % 5 == 0) ? fizzbuzz[1] : number
+# }
