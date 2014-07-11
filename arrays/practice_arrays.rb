a0830a7e0b9d3160f8bcb8edc2623a9e280fb990
Raj Solanki <rajmahaj@gmail.com>
2014-07-11 12:42:40 -0600

    changes to practice_arrays.rb based on rubocop

--- arrays/practice_arrays.rb
+++ arrays/practice_arrays.rb
@@ -15,10 +15,14 @@ puts "The third element is #{count[2]}"
 puts "The element with an index of 3 is #{count[3]}"
 puts 'The second from the last element is ' + count.reverse[1].to_s
 puts "The first four elements are '#{count[0..3].join(', ')}'"
-count.pop(6) + count.push(8, 9, 10)
-puts "If we delete 5, 6 and 7 from the array, we're left with #{count}"
-puts "If we add 5 at the beginning of the array, we're left with #{count.unshift(5)}"
+count.pop(6)
+count.push(8, 9, 10)
+print 'If we delete 5, 6 and 7 from the array, '
+puts "we're left with #{count}"
+print 'If we add 5 at the beginning of the array, '
+puts "we're left with #{count.unshift(5)}"
 puts "If we add 6 at the end fo the array, we're left with #{count.push(6)}"
-puts "Only the elements #{count[6..7]} are > #{count[5].to_s}"
+puts "Only the elements #{count[6..7]} are > #{count[5]}"
 count.pop(9)
-puts "If we remove all the elements, then the length of the array is #{count.length}"
+print 'If we remove all the elements, '
+puts "then the length of the array is #{count.length}"
