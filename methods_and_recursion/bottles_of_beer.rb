2ca8d1bfee30ddf48ba798e1074155250d7c995a
Raj Solanki <rajmahaj@gmail.com>
2014-07-12 20:47:06 -0600

    Corrections to bottles_of_beer.rb

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -5,10 +5,13 @@ def bottles(number)
     number -= 1
     plural = number == 1 ? 'bottle' : 'bottles'
     plural2 = (number.to_i - 1) == 1 ? 'bottle' : 'bottles'
-  puts "#{number} #{plural} of beer on the wall, #{number} #{plural} of beer."
-  puts "Take one down and pass it around, #{number - 1} #{plural2} of beer on the wall."
-  puts
+    puts "#{number} #{plural} of beer on the wall, #{number} #{plural} of beer."
+    print 'Take one down and pass it around,'
+    puts " #{number - 1} #{plural2} of beer on the wall."
+    puts
   end
+  puts 'No more bottles of beer on the wall, no more bottles of beer.'
+  puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
 end
 
 puts bottles(100)
