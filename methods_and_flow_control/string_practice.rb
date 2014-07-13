59bd5767b68188b693db576618ba025af7bfc087
Raj Solanki <rajmahaj@gmail.com>
2014-07-13 14:45:23 -0600

    homework corrections

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -5,7 +5,7 @@ string = 'this is a string to practice with'
 puts string
 puts string.capitalize
 puts string.upcase
-puts string.gsub('string', "'string'")
+puts string.capitalize.gsub('string', "'string'")
 puts string.gsub(string, "The string '#{string}' has #{string.count(string)} characters")
 puts string.reverse
 puts string.gsub(string, 'practice ' * 2 + 'practice!')
