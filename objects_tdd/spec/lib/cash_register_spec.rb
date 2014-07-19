cf8dfffc368715d4fdbbc143492a6c7b7fd04df5
Raj Solanki <rajmahaj@gmail.com>
2014-07-19 16:47:53 -0600

    calculator

---
+++ objects_tdd/spec/lib/cash_register_spec.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+
+describe CashRegister do
+
+  it 'should be a cash register' do
+
+    true.should == false
+  end
+end
---
+++ tdd_calculator/lib/calculator.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Calculator class
+class Calculator
+  def addition(number1, number2)
+    number1 + number2
+  end
+
+  def subtraction(number1, number2)
+    number1 - number2
+  end
+
+  def multiply(number1, number2)
+    number1 * number2
+  end
+
+  def divide(number1, number2)
+    number1 / number2
+  end
+end
