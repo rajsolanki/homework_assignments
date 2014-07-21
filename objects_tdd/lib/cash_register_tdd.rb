518ac84aed73e72ec067bb53df8fd81b0ba6390d
Raj Solanki <rajmahaj@gmail.com>
2014-07-20 19:42:06 -0600

    tdd calculator

---
+++ objects_tdd/lib/cash_register_tdd.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+# This is a cash register
+class CashRegister
+  def initialize
+    @total = 0.00
+    @purchase = 0.00
+  end
+
+  def purchase(purchase_amount)
+    @purchase = purchase_amount
+    @total += purchase_amount
+  end
+
+  def total
+    @total
+  end
+
+  def pay(payment)
+    @total -= payment
+  end
+end
+
+register = CashRegister.new
+puts register.purchase(4.00)
+puts register.purchase(5.00)
+puts register.purchase(1.00)
+puts register.pay(10.00)
+puts register.total
--- objects_tdd/spec/lib/cash_register_spec.rb
+++ objects_tdd/spec/lib/cash_register_spec.rb
@@ -1,9 +1,19 @@
 require 'rspec'
+require './lib/cash_register_tdd'
 
 describe CashRegister do
-
   it 'should be a cash register' do
-
-    true.should == false
+    expect(subject).to be_a(CashRegister)
+  end
+  it 'should add purchase amount to total' do
+    expect(subject.purchase(3.31)).to eq(3.31 + 0.00)
+  end
+  # it 'should keep a total of all purchases' do
+  #   @total = 10
+  #   expect(subject.total).to eq(10)
+  # end
+  it 'should deduct amount paid from total' do
+    @total = 10
+    expect(@total - subject.pay(10)).to eq(0)
   end
 end
