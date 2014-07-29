86e44f4890ab07f31310c53541c3fbb63a10164b
Raj Solanki <rajmahaj@gmail.com>
2014-07-28 20:06:05 -0600

    homework correction

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 
 seconds_in_minute = 60
+seconds_in_hour = 3600
+seconds_in_day = 86400
+seconds_in_week = 604800
+seconds_in_year = 31449600
 minutes_in_hour = 60
 hours_in_day = 24
 days_in_week = 7
@@ -10,14 +14,10 @@ puts "There are #{minutes_in_hour} minutes in an hour"
 puts "There are #{hours_in_day} hours in a day"
 puts "There are #{days_in_week} days in a week"
 puts 'That means there are:'
-puts "  #{seconds_in_minute * minutes_in_hour
-} seconds in an hour,"
-puts "  #{seconds_in_minute * minutes_in_hour * hours_in_day
-} seconds in a day,"
-puts "  #{seconds_in_minute * minutes_in_hour * hours_in_day *
-  days_in_week} seconds in a week"
+puts "  #{seconds_in_hour} seconds in an hour,"
+puts "  #{seconds_in_day} seconds in a day,"
+puts "  #{seconds_in_week} seconds in a week"
 puts "That means when you turn 20, you've been alive for #{
-seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week * 52 * 20} seconds,
+seconds_in_year * 20} seconds,
 and if you make it to 100, you will have lived #{
-seconds_in_minute * minutes_in_hour * hours_in_day * days_in_week * 52 * 100} seconds. Make them
-count!"
+seconds_in_year * 100} seconds. Make them count!"
